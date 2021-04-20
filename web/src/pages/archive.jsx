daacc79fc11d082bed1ef7875335141289e55dac
Jeremy Ludwig <hjeremyludwig@gmail.com>
2021-04-20 08:57:37 -0400

    Fix: update Seo component import in archive page

--- web/src/pages/archive.jsx
+++ web/src/pages/archive.jsx
@@ -1,7 +1,7 @@
 import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
-import SEO from "../components/SEO";
+import SEO from "../components/Seo";
 import styled from "styled-components";
 import { centered } from "../styles/mixins";
 import ArchiveEvent from "../components/archive/ArchiveEvent";
