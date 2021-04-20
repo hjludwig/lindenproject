847c11055c64070245aab8c900cbceda3f4f56db
Jeremy Ludwig <hjeremyludwig@gmail.com>
2021-04-20 09:57:32 -0400

    Fix: add gatsby-styled-components plugin to gatsby-config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -52,6 +52,12 @@ module.exports = {
         timeout: 3500,
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
